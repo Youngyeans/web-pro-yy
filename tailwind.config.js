5508ec0edecc884ce1ce7b9f933c4bf8e68fa7eb
youngyeans <94213667+Youngyeans@users.noreply.github.com>
2024-02-05 00:05:48 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        'rose' : '#FEAAC2',
+        'pink' : '#F775A9',
+        'yellow' : '#F9EDD1',
+        'blue' : '#8FDBD8',
+        'green' : '#10B48E',
+        'lime' : '#B8DEA4',
+        'brown': '#613B4A'
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
